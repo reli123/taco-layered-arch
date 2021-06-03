84215911072cbb814c22237f779c396a1f89d76a
Aurel Mairshta <aurel.marishta@gmail.com>
2021-06-04 01:22:44 +0200

    first commit

---
+++ taco-security/src/test/java/hr/obai/taco/security/TacoSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.obai.taco.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
